67556e640e694113c3c7c7b149ea9ca150aa335f
GuidoMartinez1 <guido.martinez20@gmail.com>
2020-06-03 01:47:57 -0300

    Punto 2 terminado

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,78 @@
 module Lib where
 import Text.Show.Functions
 
-laVerdad = True
+--PUNTO 1
+
+data Raton = Raton {
+    nombre :: String,
+    edad :: Float,
+    peso :: Float,
+    enfermedades :: Enfermedades
+}deriving (Show, Eq)
+
+type Enfermedades = [String] --por ahora
+
+cerebro :: Raton
+cerebro = Raton "Cerebro" 9 0.2 ["brucelosis","sarampion","tuberculosis"]
+
+bicenterrata :: Raton
+bicenterrata = Raton "Bicenterrata" 256 0.2 []
+
+huesudo :: Raton
+huesudo = Raton "Huesudo" 4 10 ["obesidad","sinusitis"]
+
+--PUNTO 2
+
+type Hierba = Raton -> Raton
+
+hierbaBuena :: Hierba
+hierbaBuena  = modificarEdad sqrt 
+
+modificarEdad :: (Float -> Float) -> Raton -> Raton
+modificarEdad funcion raton = raton {edad = funcion.edad $ raton}
+
+modificarEnfermedad :: (Enfermedades -> Enfermedades) -> Raton -> Raton
+modificarEnfermedad funcion raton = raton {enfermedades = funcion.enfermedades $ raton}
+
+hierbaVerde :: String -> Hierba
+hierbaVerde terminacion = modificarEnfermedad (enfermedadesSinCiertaTerminacion terminacion)
+
+enfermedadesSinCiertaTerminacion :: String -> Enfermedades -> Enfermedades
+enfermedadesSinCiertaTerminacion terminacion = filter (noTerminaEn terminacion)
+
+noTerminaEn :: String -> String->  Bool
+noTerminaEn terminacion enfermedad = (/=) terminacion (take (length terminacion) (reverse enfermedad))
+
+alcachofa :: Hierba
+alcachofa = modificarPeso perderPeso
+
+modificarPeso :: (Float -> Float) -> Raton -> Raton
+modificarPeso funcion raton = raton {peso = funcion.peso $ raton}
+
+perderPeso :: Float -> Float
+perderPeso peso
+    | (>2) peso = restarPeso 10 peso
+    | otherwise = restarPeso 5 peso
+
+restarPeso :: Float -> Float -> Float
+restarPeso porcentaje peso = peso - ((*)(porcentaje/100) peso)
+
+hierbaZort :: Hierba
+hierbaZort  = modificarNombreAPinky.eliminarTodasLasEnfermedades.modificarEdad (\edad->0)
+
+
+modificarNombreAPinky :: Raton -> Raton
+modificarNombreAPinky raton = raton {nombre = "Pinky"}
+
+eliminarTodasLasEnfermedades :: Raton -> Raton
+eliminarTodasLasEnfermedades raton = raton {enfermedades = []}
+
+hierbaDelDiablo :: Hierba
+hierbaDelDiablo = (modificarPeso perderPesoDiablo).(modificarEnfermedad enfermedadesConMenosDe10Letras)
+
+perderPesoDiablo :: Float -> Float
+perderPesoDiablo peso = max 0 (peso-0.1)
+
+enfermedadesConMenosDe10Letras :: Enfermedades -> Enfermedades
+enfermedadesConMenosDe10Letras enfermedades = filter ((<10).length) enfermedades
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,15 @@ import Lib
 
 main :: IO()
 main = hspec $ do
-   describe "Tests de prueba" $ do
-      it "la verdad y la verdad" $ do
-         laVerdad `shouldBe` True
+   describe "Aplicar Hierbas a ratones" $ do
+      it "aplicar hierbaBuena a cerebro" $ do
+         hierbaBuena cerebro `shouldBe` (Raton "Cerebro" 3 0.2 ["brucelosis","sarampion","tuberculosis"])
+      it "aplicar hierbaVerde a cerebro con la terminacion `sis`" $ do
+         hierbaVerde "sis" cerebro `shouldBe` (Raton "Cerebro" 9 0.2 ["sarampion"]) 
+      it "probar noTerminaEn `sis` la palabra Tuberculosis " $ do 
+         noTerminaEn "sis" "tuberculosis" `shouldBe` False
+      it "aplicar alcachofa a cerebro" $ do
+         alcachofa cerebro `shouldBe` (Raton "Cerebro" 9 0.19 ["brucelosis","sarampion","tuberculosis"])
+      it "aplicar hierbaDelDiablo a cerebro" $ do
+         hierbaDelDiablo cerebro `shouldBe` (Raton "Cerebro" 9 0.1 ["sarampion"])
+         
